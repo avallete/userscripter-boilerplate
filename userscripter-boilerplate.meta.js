afab8e16dc83237727a762b49fc86d2424947f37
avallete <avallete@users.noreply.github.com>
2021-03-11 09:06:18 +0000

    Deploying to gh-pages from @ avallete/userscripter-boilerplate@4f7c9cf4cab5d17fae7e8b81088821c645b5f609 🚀

---
+++ userscripter-boilerplate.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         userscripter-boirlerplate
+// @description  some description for the script
+// @version      1.0.0
+// @author       author
+// @grant        none
+// @match        *://example.cypress.io/*
+// @match        *://www.example.cypress.io/*
+// @noframes
+// @namespace    greasyfork-namespace-url
+// @homepage     https://github.com/author/my-awesome-userscript
+// @support      https://github.com/author/my-awesome-userscript/issues
+// @run-at       document-idle
+// @license      MIT
+// ==/UserScript==
