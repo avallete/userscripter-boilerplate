48c50dba58886bbff25f0b89c6a5d45d4f45f303
Andrew Valleteau <avallete@users.noreply.github.com>
2021-03-13 17:55:13 +0100

    18 use package json as single source of truth (#19)
    * refactor: use package.json and userscript as single source of truth
    
    * feat(metadata): add automatic dowloadURL from github repo url
    
    * test(ci): set NODE_ENV to test before building webextension for e2e tests
    
    * ci: setup NODE_ENV to test and production when nedeed

--- metadata.ts
+++ metadata.ts
@@ -1,38 +1,73 @@
-import { DEFAULT_METADATA_SCHEMA } from 'userscripter/build'
+import { DEFAULT_METADATA_SCHEMA, distFileName } from 'userscripter/build'
 import { Metadata, StringItem } from 'userscript-metadata'
-
 import U from './src/userscript'
+
+export type ViolentMonkeyMetadata = Metadata & {
+  name: string
+  run_at: string
+  match: string[]
+  version: string
+  author?: string
+  description?: string
+  namespace?: string
+  icon?: string
+  license?: string
+  noframes?: true
+  grant?: string
+  downloadURL?: string
+  supportURL?: string
+  homepageURL?: string
+  'exclude-match'?: string[]
+  include?: string[]
+  exclude?: string[]
+}
 
 export const MetadataSchema = {
   ...DEFAULT_METADATA_SCHEMA,
   items: {
     ...DEFAULT_METADATA_SCHEMA.items,
-    homepage: new StringItem({
-      key: 'homepage',
+    homepageURL: new StringItem({
+      key: 'homepageURL',
       unique: true,
       required: false,
     }),
-    support: new StringItem({
-      key: 'support',
+    supportURL: new StringItem({
+      key: 'supportURL',
+      unique: true,
+      required: false,
+    }),
+    downloadURL: new StringItem({
+      key: 'downloadURL',
       unique: true,
       required: false,
     }),
   },
 }
 
-export default function metadataConfigFactory(): Metadata {
+export function generateDownloadUrlFromRepositoryUrl(repositoryUrl: string): string {
+  const gitRepoExtractorRegex = /^(?:https|git|git\+ssh)[:@](?:\/\/)?[^/:]+[/:]([^/:]+\/.+).git$/
+  const match = repositoryUrl.match(gitRepoExtractorRegex)
+  if (match !== null && match.length === 2) {
+    return `https://raw.githubusercontent.com/${match[1]}/${U.releaseBranch}/${distFileName(U.id, 'user')}`
+  }
+  return ''
+}
+
+// Will generate the metadata for the code as userscript
+export default function metadataConfigFactory(): ViolentMonkeyMetadata {
   return {
     name: U.name,
     description: U.description,
     version: U.version,
     author: U.author,
-    grant: U.grant,
+    grant: 'none',
     match: [`*://${U.hostname}/*`, `*://www.${U.hostname}/*`],
-    noframes: U.noframes,
-    namespace: U.namespace,
-    homepage: U.homepage,
-    support: U.support,
-    run_at: 'document-idle',
+    namespace: 'greasyfork-namespace-url',
+    downloadURL: generateDownloadUrlFromRepositoryUrl(U.repositoryURL),
+    noframes: true,
+    homepageURL: U.homepage,
+    supportURL: U.support,
+    run_at: U.run_at,
     license: U.license,
   }
 }
--- src/userscript.ts
+++ src/userscript.ts
@@ -1,16 +1,38 @@
-export default {
-  id: 'userscripter-boilerplate',
-  name: 'userscripter-boirlerplate',
-  description: 'some description for the script',
+// Use package.json as "unique source of truth" for multiples infos
+import { Metadata } from 'userscript-metadata'
+import package_json from '../package.json'
+
+export type UserscriptMetadata = Metadata & {
+  id: string
+  name: string
+  description: string
+  version: string
+  author: string
+  homepage: string
+  support: string
+  hostname: string
+  sitename: string
+  repositoryURL: string
+  license: string
+  run_at: string
+  releaseBranch: string
+}
+
+const userscriptMetadata: UserscriptMetadata = {
+  id: package_json.name,
+  name: package_json.name,
+  description: package_json.description,
   // Automatically match the userscript metadata version to the package.json one
-  version: process.env.npm_package_version as string,
-  author: 'author',
-  homepage: 'https://github.com/author/my-awesome-userscript',
-  support: 'https://github.com/author/my-awesome-userscript/issues',
-  grant: 'none',
+  version: package_json.version,
+  author: package_json.author,
+  homepage: package_json.homepage,
+  support: package_json.bugs.url,
   hostname: 'example.cypress.io',
-  sitename: 'myuserscript',
-  namespace: 'greasyfork-namespace-url',
-  noframes: true,
-  license: 'MIT',
-} as const
+  sitename: package_json.repository.url,
+  repositoryURL: package_json.repository.url,
+  license: package_json.license,
+  run_at: 'document-idle',
+  releaseBranch: 'gh-pages',
+}
+
+export default userscriptMetadata
