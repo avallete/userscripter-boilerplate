b00b186d3ccb87fd3467449019f05ca90658cc85
avallete <avallete@student.42.fr>
2021-03-17 16:28:28 +0100

    test(e2e): add minimal test to check if the script has been run on the page

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
+import U from '~src/userscript'
+
 describe('test extension', () => {
+  it('should load extension set run attr to <html> tag', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('html').should('have.attr', `data-${U.id}-has-run`)
+  })
   it('should change utilities page background color after extension load', () => {
     cy.visit('https://example.cypress.io/')
     // if our webextension is loaded the background color should have been changed
